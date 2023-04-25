3cadc03c8701c12e44ca19b6473214131462c0ee
Bruno Rosendo <brunomrosendo@hotmail.com>
2023-04-25 18:30:26 +0100

    temporary timer

--- kernel.py
+++ kernel.py
@@ -19,7 +19,7 @@ def t3():
 
 def set_interrupts():
     global timer
-    timer = threading.Timer(0.001, Sched_Interrupt)
+    timer = threading.Timer(0.1, Sched_Interrupt)
     timer.start()
 
 def stop_interrupts():
@@ -27,72 +27,56 @@ def stop_interrupts():
     timer.cancel()
 
 def Sched_Init():
-    print("initing scheduler")
     # TODO configure interrupts and stuff on PI
     set_interrupts()
     return
 
 def Sched_AddTask(func, delay, period):
-    print("adding task")
     global current_task
     tasks.append(Task(func, delay, period))
     current_task = len(tasks)
 
 def Sched_Schedule():
-    print("scheduling")
     for task in tasks:
         if task.func == None:
-            print("no function")
             continue
         if task.delay > 0:
-            print("delaying ", task.delay)
             task.delay -= 1
         else:
-            print("counting")
             task.counter += 1
             task.delay = task.period - 1
 
 def Sched_Dispatch():
     global current_task
     prev_task = current_task
-    print("dispatching")
     for i in range(current_task):
-        print(0)
         task = tasks[i]
         if task.func == None or task.counter == 0:
             continue
-        print(1)
         task.counter = 0
-        print(2)
         current_task = i
-        print(3)
 
         # enable interrupts
         set_interrupts()
-        print(4)
         task.func()
-        print(5)
         # disable interrupts
         stop_interrupts()
-        print(6)
 
         current_task = prev_task
-        print(7)
 
         # one shot task
         if task.period == 0:
             tasks.remove(task)
-        print(8)
 
 def setup():
     print("setting up")
     # TODO initialize pins
     Sched_Init()
 
-    # TODO add tasks
-    Sched_AddTask(t1, 0, 1000)
-    Sched_AddTask(t2, 0, 5000)
-    Sched_AddTask(t3, 0, 10000)
+    # TODO this is using seconds but needs to use ms later
+    Sched_AddTask(t1, 0, 10)
+    Sched_AddTask(t2, 0, 50)
+    Sched_AddTask(t3, 0, 100)
 
 def Sched_Interrupt():
     stop_interrupts()
