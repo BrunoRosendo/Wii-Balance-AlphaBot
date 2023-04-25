2da60fa5c2f657dcba47d26a0535a8e1f26cc5db
Bruno Rosendo <brunomrosendo@hotmail.com>
2023-04-25 18:02:54 +0100

    timer not working in 1ms

--- kernel.py
+++ kernel.py
@@ -1,6 +1,7 @@
 from task import Task
 import threading
 
+timer = None
 tasks = []
 current_task = 0
 
@@ -16,45 +17,72 @@ def t3():
     print("hello world 10secs")
     return
 
-def set_timer():
-    threading.Timer(1, Sched_Interrupt).start()
+def set_interrupts():
+    global timer
+    timer = threading.Timer(0.001, Sched_Interrupt)
+    timer.start()
+
+def stop_interrupts():
+    global timer
+    timer.cancel()
 
 def Sched_Init():
     print("initing scheduler")
     # TODO configure interrupts and stuff on PI
-    set_timer()
+    set_interrupts()
     return
 
 def Sched_AddTask(func, delay, period):
     print("adding task")
+    global current_task
     tasks.append(Task(func, delay, period))
+    current_task = len(tasks)
 
 def Sched_Schedule():
     print("scheduling")
     for task in tasks:
         if task.func == None:
+            print("no function")
             continue
         if task.delay > 0:
+            print("delaying ", task.delay)
             task.delay -= 1
         else:
+            print("counting")
             task.counter += 1
             task.delay = task.period - 1
 
 def Sched_Dispatch():
-    print("dispatching")
     global current_task
     prev_task = current_task
-    for (i, task) in enumerate(tasks):
+    print("dispatching")
+    for i in range(current_task):
+        print(0)
+        task = tasks[i]
         if task.func == None or task.counter == 0:
             continue
+        print(1)
         task.counter = 0
+        print(2)
         current_task = i
+        print(3)
+
         # enable interrupts
+        set_interrupts()
+        print(4)
         task.func()
+        print(5)
         # disable interrupts
+        stop_interrupts()
+        print(6)
+
         current_task = prev_task
+        print(7)
+
+        # one shot task
         if task.period == 0:
             tasks.remove(task)
+        print(8)
 
 def setup():
     print("setting up")
@@ -67,14 +95,12 @@ def setup():
     Sched_AddTask(t3, 0, 10000)
 
 def Sched_Interrupt():
-    print("interrupting")
+    stop_interrupts()
     Sched_Schedule()
     Sched_Dispatch()
-    set_timer()
+    set_interrupts()
 
 setup()
 
 while True:
     pass # keep it running
-
-timer.deinit()
